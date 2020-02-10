046e5d8f31236070f4ebe1910d1b93f25f570f83
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-02-09 21:36:24 -0500

    Add files via upload
    Finds the variables in the .py file and creates a table with the class name, variables and methods.

--- UML-Maker.py
+++ UML-Maker.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt 
+
 classes = []
 methods = []
+var = []
 temp = []
 no_spaces = ""
+
 code = open("example.py", 'r')
 
 for line in code:
-    
     if "class" in line:
         name = []
         no_spaces = line
@@ -32,7 +35,22 @@ for line in code:
                 break
         methods[len(methods)-1] = "".join(name)
 
-print("These are the classes in this file:")
+    elif "=" in line:
+        if len(line.lstrip()) == len(line):
+            name = []
+            no_spaces = line
+            no_spaces = no_spaces.strip()
+            temp = no_spaces.split(" ")
+            var.append(temp[0])
+            print(var)
+            for index in var[len(var)-1]:
+                if index != "=":
+                    name.append(index)
+                else:
+                    break
+            var[len(var)-1] = "".join(name)
+            
+"""print("These are the classes in this file:")
 for name in classes:
     print(name)
 
@@ -40,5 +58,26 @@ print("These are the methods in this file:")
 for name in methods:
     print(name)
 
+print("These are the variables in this file:")
+for name in var:
+    print(name)"""
 
-    
+fig = plt.figure(dpi=80)
+ax = fig.add_subplot(1,1,1)
+table_data=[
+    [classes[0]],
+    [var],
+    [methods]
+]
+
+"""for name in var:
+    table_data[1].append(name)
+
+for name in methods:
+    table_data[2].append(name)"""
+
+table = ax.table(cellText=table_data, loc='center')
+table.set_fontsize(14)
+table.scale(1,4)
+ax.axis('off')
+plt.show()
